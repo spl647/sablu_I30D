c21e7cb3ea7d58804b8a61c1fa566f53410bd067
spl647 <slu3601@gmail.com>
2024-04-12 10:08:13 -0500

    added volume_of_sphere.py

---
+++ volume_of_sphere.py
@@ -0,0 +1,13 @@
+def calculate_volume_of_sphere(radius):
+    vol = (4/3)*3.14*(radius**3)
+    return vol
+
+r1 = 30
+vol1 = calculate_volume_of_sphere(r1)
+print(f"The volumne of sphere with radius {r1} is: {vol1}")
+
+r2 = 40
+vol2 = calculate_volume_of_sphere(r2)
+print(f"The volumne of sphere with radius {r2} is: {vol2}")
+
+
